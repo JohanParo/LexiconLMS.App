ba03c7f8c891d2fff1f5e0b88cee52b779dc8b6f
johan <jparo@kth.se>
2023-03-03 14:26:55 +0100

    some fixes to document class

--- LexiconLMS.App/Shared/Entities/Document.cs
+++ LexiconLMS.App/Shared/Entities/Document.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,20 @@ namespace LexiconLMS.Shared.Entities
 {
 	public class Document
 	{
+		[Key]
 		public int Id { get; set; }
-		public int CMAID { get; set; }
+		public int? CMAID { get; set; }
 		public string Title { get; set; }
 		public DocumentType DocumentType { get; set; }
-		public string Description { get; set; }
+		public string? Description { get; set; }
 		public DateTime ShowTime { get; set; }
 		public bool IsHidden { get; set; }
 		public string DocumentFile{ get; set; }
+		[Required]
 		public int UserId { get; set; }
+
+		public int? CourseId { get; set; }
+		public int? ModuleId { get; set; }
+		public int? ActivityId { get; set; }
 	}
 }
