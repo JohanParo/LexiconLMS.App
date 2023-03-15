6e6714d8485712baa4dddeb93d76803e841710ae
johan <jparo@kth.se>
2023-03-15 13:40:28 +0100

    Added API validation requirements to models

--- LexiconLMS.App/Server/Models/Course.cs
+++ LexiconLMS.App/Server/Models/Course.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace LexiconLMS.App.Server.Models
 {
@@ -10,9 +13,13 @@ namespace LexiconLMS.App.Server.Models
     public class Course
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        [Required]
         public DateTime StartTime { get; set; }
+        [Required]
         public DateTime EndTime { get; set; }
         public List<Module> Modules { get; set; }
         public List<ApplicationUser> Users { get; set; }
--- LexiconLMS.App/Server/Models/Module.cs
+++ LexiconLMS.App/Server/Models/Module.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace LexiconLMS.App.Server
 {
@@ -10,11 +13,16 @@ namespace LexiconLMS.App.Server
 	public class Module
 	{
 		public int Id { get; set; }
-		public string Title { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Title { get; set; } = string.Empty;
 		public string Description { get; set; } = string.Empty;
-		public DateTime StartTime { get; set; }
-		public DateTime EndTime { get; set; }
-		public int CourseId { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public int CourseId { get; set; }
 		public List<Activity> Activities { get; set; }
         public bool Published { get; set; } = true;
     }
