8c869abfb8ccb46f0ef41bbb1999796de17f53ca
robboten <robboten@gmail.com>
2023-03-21 10:07:11 +0100

    test page

--- LexiconLMS.App/Client/Models/ModalForm.cs
+++ LexiconLMS.App/Client/Models/ModalForm.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; } = "Edit";
         public ModalType Type { get; set; }
+        public int? ParentId { get; set; }
 
 
     }
